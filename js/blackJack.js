f5f84e36701ec20cfe98133d96f21b918f99776f
robin <algofribaz@maison.fr>
2017-11-19 17:49:14 +0100

    correction bogues/ajustements html

--- js/blackJack.js
+++ js/blackJack.js
@@ -9,13 +9,17 @@ function playGame() {
   var robotNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   if (humanNumber.value == "") {
     alert("Indique un nombre bon sang!")
-  } else {
+  }
+  else if (humanNumber.value > 10 | humanNumber.value < 1) {
+    alert("Indique un nombre compris entre 1 et 10, moule à gaufre!")
+  }
+  else {
     if (humanNumber.value > robotNumber) {
       alert("Tu as joué le " + humanNumber.value + " et j'ai joué le " + robotNumber + " ,sois maudit humain! Tu as gagné !");
-    } else {
+    }
+    else {
       alert("Tu as joué le " + humanNumber.value + " et j'ai joué le " + robotNumber + " ,tu as perdu, comme je l'avais prévu!");
     }
-
   }
 
 
