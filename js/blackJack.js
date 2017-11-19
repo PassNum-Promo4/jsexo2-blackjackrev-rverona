d6d065f05564d1c70d1b31f52d0ed8ad10f8caa2
robin <algofribaz@maison.fr>
2017-11-19 16:21:56 +0100

    Intégration bootstrap, le jeu fonctionne

--- js/blackJack.js
+++ js/blackJack.js
@@ -1,14 +1,24 @@
 // Fonction random pour générer le chiffre du robot au moment où l'humain clique sur le bouton
-function getRandomIntInclusive() {
+var humanNumber = document.getElementById("humanNumber");
+var resultDuel = document.getElementById("human__number--result");
+
+
+function playGame() {
   var min = Math.ceil(0);
   var max = Math.floor(10);
   var robotNumber = Math.floor(Math.random() * (max - min +1)) + min;
-  var humanNumber = document.getElementById("humanNumber").value;
-  document.getElementById("human__number--result").innerHTML = "Vous avez joué le " + humanNumber + " et j'ai joué le " + robotNumber;
-}
+  if (humanNumber.value == "") {
+    alert("Indiquez un nombre !")
+  }
+  else {
+    resultDuel.innerHTML = "Vous avez joué le " + humanNumber.value + " et j'ai joué le " + robotNumber;
 
-//affichage du chiffre de l'humain
-// function displayHumanNumber() {
-//   var humanNumber = document.getElementById("humanNumber").value;
-//   document.getElementById("human__number--result").innerHTML = "Vous avez joué le " + humanNumber;
-// }
+  }
+  if (humanNumber.value > robotNumber) {
+    alert("Sois maudit humain! Tu as gagné !");
+  }
+  else {
+    alert("Tu as perdu, comme je l'avais prévu!");
+  }
+
+}
